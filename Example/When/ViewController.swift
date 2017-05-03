cfaf4ae0c047e56cece83ff9fcd30e6d63b30606
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2017-05-03 20:15:27 +0300

    Initial commit

---
+++ Example/When/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  When
+//
+//  Created by SiarheiFedartsou on 05/03/2017.
+//  Copyright (c) 2017 SiarheiFedartsou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
